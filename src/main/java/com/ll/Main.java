af0adfc082c9ecc9cb50d4c7f9b6844707e23e04
gaeun220 <gaeun2701@gmail.com>
2024-11-20 16:41:48 +0900

    30-4

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -6,23 +6,19 @@ import com.sun.source.doctree.SystemPropertyTree;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Person p1 = new Person();
-        p1.나이 = 40;
-        p1.자기소개();
-
-        Person.인류평균나이 = 37;
-        Person.인류평균나이소개();
+        자동차 a자동차;
+        a자동차 = new 자동차();
+        a자동차.달리다();
+        a자동차.정지하다();
     }
 }
 
-class Person {
-    int 나이;
-    static int 인류평균나이;
-
-    void 자기소개() {
-        System.out.println("제 나이는 " + this.나이 + "살 입니다.");
+class 자동차 {
+    void 달리다() {
+        System.out.println("자동차가 달립니다.");
     }
-    static void 인류평균나이소개() {
-        System.out.println("인류평균나이는 " + 인류평균나이 + "살 입니다.");
+
+    void 정지하다() {
+        System.out.println("자동차가 정지합니다.");
     }
 }
